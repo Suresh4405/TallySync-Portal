9bda33751e2052d6cd29d97a89f04bc2f09abff4
Suresh4405 <m.suresh4405@gmail.com>
2025-12-10 16:04:14 +0530

    added env files

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
+ server: {
+  port: 3000,
+  proxy: {
+    '/api': {
+      target: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, '')
     }
   }
+}
+
 })
